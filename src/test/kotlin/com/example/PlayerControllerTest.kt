0d1f6befe2b06b49995de454e5753359035b29ef
Yohan Siguret <yohan.siguret.uncork171@slmail.me>
2024-10-15 12:44:53 +0200

    test: cleanup

--- src/test/kotlin/com/example/PlayerControllerTest.kt
+++ src/test/kotlin/com/example/PlayerControllerTest.kt
@@ -4,152 +4,77 @@ import com.example.model.PlayerDtoCreate
 import com.example.model.PlayerDtoUpdate
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import io.restassured.http.ContentType
+import io.restassured.response.ValidatableResponse
 import io.restassured.specification.RequestSpecification
 import org.hamcrest.Matchers.equalTo
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 
 @MicronautTest
-class PlayerControllerTest {
+class PlayerControllerTest(val spec: RequestSpecification) {
+
+    @AfterEach
+    fun cleanup() {
+        spec.`when`()
+            .delete("/players")
+            .then()
+            .statusCode(200)
+    }
 
     @Test
-    fun fullScenario(spec: RequestSpecification) {
+    fun fullScenario() {
         val nick1 = "bob"
         val nick2 = "tim"
 
-        spec
-            .`when`()
-            .get("/players")
-            .then()
-            .statusCode(200)
+        spec.getAllPlayers()
             .body("size()", equalTo(0))
 
-        spec
-            .given()
-            .body(PlayerDtoCreate(nick1))
-            .contentType(ContentType.JSON)
-            .`when`()
-            .post("/players")
-            .then()
-            .statusCode(200)
+        spec.createPlayer(nick1)
 
-        spec
-            .given()
-            .body(PlayerDtoCreate(nick2))
-            .contentType(ContentType.JSON)
-            .`when`()
-            .post("/players")
-            .then()
-            .statusCode(200)
+        spec.createPlayer(nick2)
 
-        spec
-            .`when`()
-            .get("/players")
-            .then()
-            .statusCode(200)
+        spec.getAllPlayers()
             .body("size()", equalTo(2))
 
-        spec
-            .`when`()
-            .get("/players/$nick1")
-            .then()
-            .statusCode(200)
+        spec.getPlayer(nick1)
             .body("nickname", equalTo(nick1))
             .body("score", equalTo(0))
 
-        spec
-            .given()
-            .body(PlayerDtoUpdate(2))
-            .contentType(ContentType.JSON)
-            .`when`()
-            .put("/players/$nick1")
-            .then()
-            .statusCode(200)
+        spec.updatePlayerScore(nick1, 2)
 
-        spec
-            .`when`()
-            .get("/players/$nick1")
-            .then()
-            .statusCode(200)
+        spec.getPlayer(nick1)
             .body("nickname", equalTo(nick1))
             .body("score", equalTo(2))
 
-        spec
-            .`when`()
+        spec.`when`()
             .delete("/players")
             .then()
             .statusCode(200)
 
-        spec
-            .`when`()
-            .get("/players")
-            .then()
-            .statusCode(200)
+        spec.getAllPlayers()
             .body("size()", equalTo(0))
     }
 
     @Test
-    fun shouldBeOrderByScoreDescAndHaveARank(spec: RequestSpecification) {
+    fun shouldBeOrderedByScoreDescAndHaveRank() {
         val nick1 = "bob"
         val nick2 = "lea"
         val nick3 = "tim"
 
-        spec
-            .`when`()
-            .get("/players")
-            .then()
-            .statusCode(200)
+        spec.getAllPlayers()
             .body("size()", equalTo(0))
 
-        spec
-            .given()
-            .body(PlayerDtoCreate(nick1))
-            .contentType(ContentType.JSON)
-            .`when`()
-            .post("/players")
-            .then()
-            .statusCode(200)
+        spec.createPlayer(nick1)
 
-        spec
-            .given()
-            .body(PlayerDtoCreate(nick2))
-            .contentType(ContentType.JSON)
-            .`when`()
-            .post("/players")
-            .then()
-            .statusCode(200)
+        spec.createPlayer(nick2)
 
-        spec
-            .given()
-            .body(PlayerDtoCreate(nick3))
-            .contentType(ContentType.JSON)
-            .`when`()
-            .post("/players")
-            .then()
-            .statusCode(200)
+        spec.createPlayer(nick3)
 
-        spec
-            .given()
-            .body(PlayerDtoUpdate(2))
-            .contentType(ContentType.JSON)
-            .`when`()
-            .put("/players/$nick2")
-            .then()
-            .statusCode(200)
+        spec.updatePlayerScore(nick2, 2)
 
-        spec
-            .given()
-            .body(PlayerDtoUpdate(1))
-            .contentType(ContentType.JSON)
-            .`when`()
-            .put("/players/$nick1")
-            .then()
-            .statusCode(200)
+        spec.updatePlayerScore(nick1, 1)
 
-        spec
-            .`when`()
-            .get("/players")
-            .then()
-            .statusCode(200)
+        spec.getAllPlayers()
             .body("size()", equalTo(3))
             .body("[0].nickname", equalTo(nick2))
             .body("[0].score", equalTo(2))
@@ -161,30 +86,50 @@ class PlayerControllerTest {
             .body("[2].score", equalTo(0))
             .body("[2].rank", equalTo(3))
 
-        spec
-            .`when`()
-            .get("/players/$nick2")
-            .then()
-            .statusCode(200)
+        spec.getPlayer(nick2)
             .body("rank", equalTo(1))
 
-        spec
-            .`when`()
-            .get("/players/$nick1")
-            .then()
-            .statusCode(200)
+        spec.getPlayer(nick1)
             .body("rank", equalTo(2))
 
-        spec
+        spec.getPlayer(nick3)
+            .body("rank", equalTo(3))
+    }
+
+    private fun RequestSpecification.getAllPlayers(): ValidatableResponse {
+        return this
             .`when`()
-            .get("/players/$nick3")
+            .get("/players")
             .then()
             .statusCode(200)
-            .body("rank", equalTo(3))
+    }
 
-        spec
+    private fun RequestSpecification.getPlayer(nickname: String): ValidatableResponse {
+        return this
             .`when`()
-            .delete("/players")
+            .get("/players/${nickname}")
+            .then()
+            .statusCode(200)
+    }
+
+    private fun RequestSpecification.createPlayer(nickname: String): ValidatableResponse {
+        return this
+            .given()
+            .body(PlayerDtoCreate(nickname))
+            .contentType(ContentType.JSON)
+            .`when`()
+            .post("/players")
+            .then()
+            .statusCode(200)
+    }
+
+    private fun RequestSpecification.updatePlayerScore(nickname: String, newScore: Int): ValidatableResponse {
+        return this
+            .given()
+            .body(PlayerDtoUpdate(newScore))
+            .contentType(ContentType.JSON)
+            .`when`()
+            .put("/players/$nickname")
             .then()
             .statusCode(200)
     }
